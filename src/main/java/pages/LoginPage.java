dbcf8296ce3e95639669f4825f6e5d9493896f22
Sassri24 <chamodganegoda@gmail.com>
2024-12-16 19:57:39 +0530

    complete TC002 and login successfully

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -1,4 +1,32 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class LoginPage {
+
+    WebDriver driver;
+
+    public LoginPage(WebDriver driver){
+        this.driver=driver;
+    }
+
+    By signIn_Loc= By.linkText("sign-in");
+    By userName_Loc=By.xpath("//input[@name='userName']");
+    By password_Loc=By.xpath("//input[@name='password']");
+    By submitBtn_Loc=By.xpath("//input[@name='submit']");
+
+
+    public void clickSignIn(){
+        driver.findElement(signIn_Loc).click();
+    }
+    public void setUserName(String userName){
+        driver.findElement(userName_Loc).sendKeys(userName);
+    }
+    public void setPassword(String password){
+        driver.findElement(password_Loc).sendKeys(password);
+    }
+    public void clickSubmitBtn(){
+        driver.findElement(submitBtn_Loc).click();
+    }
 }
--- src/main/java/pages/LoginSuccessPage.java
+++ src/main/java/pages/LoginSuccessPage.java
@@ -1,4 +1,22 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
 public class LoginSuccessPage {
+
+    WebDriver driver;
+
+    public LoginSuccessPage(WebDriver driver){
+        this.driver=driver;
+    }
+
+    By loginSuccessful_Loc= By.xpath("//h3[normalize-space()='Login Successfully']");
+
+    public String checkLoginSuccess(){
+        String successMessage= driver.findElement(loginSuccessful_Loc).getText();
+        return successMessage;
+    }
 }
