4fcad48c1a098ecbe5628828e76218574638278b
Sassri24 <chamodganegoda@gmail.com>
2024-12-16 19:02:20 +0530

    complete TC001

--- src/main/java/pages/RegisterSuccessPage.java
+++ src/main/java/pages/RegisterSuccessPage.java
@@ -1,4 +1,19 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class RegisterSuccessPage {
+
+    WebDriver driver;
+
+    public RegisterSuccessPage(WebDriver driver){
+        this.driver=driver;
+    }
+    By checkSuccess_Loc= By.xpath("//b[contains(text(),'Dear')]");
+
+    public String registerSuccessText(){
+        String successText=driver.findElement(checkSuccess_Loc).getText();
+        return successText;
+    }
 }
--- src/test/java/testCases/TC001_RegisterUserTest.java
+++ src/test/java/testCases/TC001_RegisterUserTest.java
@@ -2,11 +2,13 @@ package testCases;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pages.HomePage;
 import pages.RegisterPage;
+import pages.RegisterSuccessPage;
 
 import java.time.Duration;
 
@@ -38,6 +40,10 @@ public class TC001_RegisterUserTest {
         registerPage.setConfirmPassword("12345678");
 
         registerPage.clickSubmitBtn();
+
+        RegisterSuccessPage registerSuccessPage=new RegisterSuccessPage(driver);
+        String actualText=registerSuccessPage.registerSuccessText();
+        Assert.assertTrue(actualText.contains("Dear"),"Registration attempt failed");
     }
 
     @AfterMethod
